7b2484e39ca76fdf24fcf293d14f7f59c77a91a1
Nelsoon-ds <davidgaming108@gmail.com>
2024-11-22 14:26:27 +0100

    feat(js-exercises): 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num)
+{
+    var repeatedString = '';
+while (num > 0) {
+    repeatedString+= string;
+    num --;
+}
+return repeatedString
 
-};
-
+}
 // Do not edit below this line
 module.exports = repeatString;
