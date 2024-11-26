4fae5cbbf793c6e7276546ad8f58e1c78c37f02e
Nelsoon-ds <davidgaming@hotmail.dk>
2024-11-26 18:57:32 +0100

    feat(tests): added the test changes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (a < 0 || b < 0) return "ERROR";
+    if (typeof a !== 'number' || typeof b !== 'number') return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    
+  const lower = Math.min(a, b);
+  const upper = Math.max(a, b);
 
+
+  let sum = 0;
+
+  for (let i = lower; i <= upper; i++) {
+        sum += i;
+    }
+  return sum;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  total = num1 + num2;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  total = num1 - num2;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let total = 1;
+  arr.map((currItem) => {
+    total *= currItem;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function (num, exponent) {
+  var result = 1;
+
+  for (let i = 0; i < exponent; i++) {
+    result = num ** exponent;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1;
+  for (let i = 0; i < num; i++) {
+    total = num * total;
+    num ++;
+    
+  }
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +50,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
